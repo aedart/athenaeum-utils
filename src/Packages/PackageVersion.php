824a438044ad3d0f64bbadac0ac0740b7fe2307f
aedart <aedart@gmail.com>
2023-01-19 10:34:42 +0000

    Merge b1f3d050e94168826126f15f6e0a3fe44f79ed6f into e98019daa7e813f2d6597fb23cf881a4a226f3cb

--- src/Packages/PackageVersion.php
+++ src/Packages/PackageVersion.php
@@ -27,8 +27,7 @@ class PackageVersion implements Version
         protected string $version,
         protected string|null $fullVersion = null,
         protected string|null $reference = null
-    ) {
-    }
+    ) {}
 
     /**
      * @inheritDoc
